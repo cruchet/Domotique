0087b514f08795d409b63da415a860b5752647c7
jordan Metz <jordan.metz@epfl.ch>
2016-11-23 11:12:49 +0100

    control

--- Domotique/Control.cpp
+++ Domotique/Control.cpp
@@ -4,7 +4,6 @@
  *  Created on: 14 nov. 2016
  *      Author: Vassili
  */
-/*heheeheh*/
 
 #include "Processus.h"
 #include "Control.h"
@@ -14,29 +13,15 @@ using namespace std;
 
 namespace Domotique {
 
-Control::Control(): Processus("ctrl"), etat_courant_(0), val_sat_(10) {}
+Control::Control(): Processus("ctrl"), valsat_(10) {}
 
 void Control::run(void) {
-	Serveur& serveur = static_cast<Serveur&>(this->get_refserveur());
-	Etat& etat = static_cast<Etat&>(this->get_refetat());
-
-	double valphen = etat.get_valphen();
-	double etat_courant =etat.get_etat_courant();
-
-	set_valphen(valphen);
-	serveur.save_valphen(valphen);
-	set_etat_courant(etat_courant);
-	serveur.save_etat_courant(etat_courant);
-
-	double valctrl = calcul_valctrl(etat_courant_);
-
-	serveur.save_valctrl(valctrl);
-	etat.set_valctrl(valctrl);
+	param_.at(VALCTRL) = calcul_valctrl(param_.at(ETAT_COURANT));
 }
 double Control::calcul_valctrl(double etat_courant) {
 	double valctrl;
-	if(etat_courant > val_sat_)
-		valctrl = val_sat_;
+	if(etat_courant > valsat_)
+		valctrl = valsat_;
 	else
 		valctrl = etat_courant;
 	return valctrl;
