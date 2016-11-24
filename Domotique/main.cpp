29913ec8311182eae3f8e326f6abb207bf578796
Vassili <Vassili@128.179.142.169>
2016-11-24 09:15:40 +0100

    type_phen => mode_phen

--- Domotique/main.cpp
+++ Domotique/main.cpp
@@ -39,7 +39,7 @@ int main(void) {
 	}
 
 	TiXmlElement* child1 = doc.FirstChildElement();
-	//méthode d'élément permet d'accéder au premier fils de l'arborescence qui est l'élément Paysage
+	//methode d'element permet d'acceder au premier fils de l'arborescence qui est l'element Paysage
 
 	//--------------------- EXTRACTION DES TRIPLETS: NIVEAU 1
 	for(TiXmlElement* child2 = child1->FirstChild("zone")->ToElement(); child2; child2=child2->NextSiblingElement())
@@ -52,45 +52,45 @@ int main(void) {
 				// EXTRACTION DU PHENOMENE: NIVEAU 2
 				TiXmlElement* child3 = child2->FirstChild("phenomene")->ToElement () ;
 				string nom_phen = child3->Attribute( "nom");
-				string type_phen=  child3->Attribute( "type");
-				if (strcmp(child3->Attribute("type"),"aleatoire")==0){
+				string mode_phen=  child3->Attribute( "mode");
+				if (strcmp(child3->Attribute("mode"),"aleatoire")==0){
 					TiXmlElement* child4 = child3->FirstChild("parametres")->ToElement();
 					vector<double> phen_param(2);
 					phen_param.at(0) = get_attr_dbl(child4,"valmin", true, 1);
 					phen_param.at(1) =  get_attr_dbl(child4,"valmax", true, 10);
-					Phenomene* phen = new Phenomene(nom_phen, type_phen, phen_param);
+					Phenomene* phen = new Phenomene(nom_phen, mode_phen, phen_param);
 					zone.push_back(phen);
 				}
 				/*----------------------POUR RENDU 2:
 				// EXTRACTION DES PARAMETRES DU PHENOMENE SELON SON TYPE ---------
-				if (strcmp(child3->Attribute("type"),"sinus")==0){ // Détection d'un phénomène sinusoïdal
+				if (strcmp(child3->Attribute("type"),"sinus")==0){ // Detection d'un phenomene sinusoidal
 				TiXmlElement* child4 = child3->FirstChild("parametres")->ToElement();
-				// EXTRACTION seule série de paramètre pour le sinu: NIVEAU 4
-				cout << "offset phénomène : " << child4->Attribute( "offset") <<endl;
-				// EXTRACTION paramètre offset du phenomene:NIVEAU 5
-				cout << "amplitude du phénomène : " << child4->Attribute( "ampl") <<endl;
-				// EXTRACTION paramètre ampl du phenomene: NIVEAU 5
-				cout << "période du phénomène : " << child4->Attribute( "periode") <<endl;
-				// EXTRACTION paramètre periode du phenomene: NIVEAU 5
-				cout << "étendue maximale du phénomène : " << child4->Attribute( "em") <<endl;
-				// EXTRACTION paramètre em du phenomene: NIVEAU 5
-				cout << "zéro décalé du phénomène : " << child4->Attribute( "zerodecale") <<endl;
-				// EXTRACTION paramètre zerodecale du phenomene: NIVEAU 5
+				// EXTRACTION seule serie de parametre pour le sinu: NIVEAU 4
+				cout << "offset phenomene : " << child4->Attribute( "offset") <<endl;
+				// EXTRACTION parametre offset du phenomene:NIVEAU 5
+				cout << "amplitude du phenomene : " << child4->Attribute( "ampl") <<endl;
+				// EXTRACTION parametre ampl du phenomene: NIVEAU 5
+				cout << "periode du phenomene : " << child4->Attribute( "periode") <<endl;
+				// EXTRACTION parametre periode du phenomene: NIVEAU 5
+				cout << "etendue maximale du phenomene : " << child4->Attribute( "em") <<endl;
+				// EXTRACTION parametre em du phenomene: NIVEAU 5
+				cout << "zero decale du phenomene : " << child4->Attribute( "zerodecale") <<endl;
+				// EXTRACTION parametre zerodecale du phenomene: NIVEAU 5
 				}
-				else if (strcmp(child3->Attribute("type"),"trapeze")==0){ // Détection d'un phénomène trapézoïdal
+				else if (strcmp(child3->Attribute("type"),"trapeze")==0){ // Detection d'un phenomene trapezoïdal
 					for(TiXmlElement * child4 = child3->FirstChild("parametres")->ToElement(); child4; child4=child4->NextSiblingElement())
-					{ 	//BOUCLE pour extraire les séries de "parametres" du phenomene.
+					{ 	//BOUCLE pour extraire les series de "parametres" du phenomene.
 						//Il y en a plusieurs pour le trapeze: NIVEAU 4
-						cout << "tranche du phénomène : " << child4->Attribute( "tranche") <<endl;
-						// EXTRACTION paramètre tranche du phenomene: NIVEAU 5
-						cout << "début de la tranche en mn : " << child4->Attribute( "debut") <<endl;
-						// EXTRACTION paramètre debut du phenomene:NIVEAU 5
+						cout << "tranche du phenomene : " << child4->Attribute( "tranche") <<endl;
+						// EXTRACTION parametre tranche du phenomene: NIVEAU 5
+						cout << "debut de la tranche en mn : " << child4->Attribute( "debut") <<endl;
+						// EXTRACTION parametre debut du phenomene:NIVEAU 5
 						cout << "fin de la tranche en minutes : " << child4->Attribute( "fin") <<endl;
-						// EXTRACTION paramètre fin du phenomene: NIVEAU 5
+						// EXTRACTION parametre fin du phenomene: NIVEAU 5
 						cout << "pente de la tranche : " << child4->Attribute( "pente") <<endl;
-						// EXTRACTION paramètre pente du phenomene:NIVEAU 5
+						// EXTRACTION parametre pente du phenomene:NIVEAU 5
 						cout << "offset de la tranche : " << child4->Attribute( "offset") <<endl;
-						// EXTRACTION paramètre ooffset du phenomene: NIVEAU 5
+						// EXTRACTION parametre ooffset du phenomene: NIVEAU 5
 					}
 				}--------------FIN POUR RENDU 2 ---------------------------*/
 
@@ -109,18 +109,18 @@ int main(void) {
 				TiXmlElement* child6 = child2->FirstChild("controle")->ToElement();
 				vector<double> param_ctrl(0);
 				string nom_ctrl = child6->Attribute( "nom");
-				string type=  child6->Attribute( "type");
-				if (strcmp(child6->Attribute("type"),"saturation")==0){
+				string mode_ctrl=  child6->Attribute( "mode");
+				if (strcmp(child6->Attribute("mode"),"saturation")==0){
 					TiXmlElement* child7 = child6->FirstChild("parametres")->ToElement();
 					param_ctrl.push_back (get_attr_dbl(child7,"valsat", true, 1));
 				}
-				Control* ctrl = new Control(nom_ctrl, type, param_ctrl);
+				Control* ctrl = new Control(nom_ctrl, mode_ctrl, param_ctrl);
 				zone.push_back(ctrl);
 
 				paysage.push_back(zone);
 			}
 			else cout << "Ce n'est pas une zone, il s'agit de la balise : " << child2->Value() << endl;
-			// élément autre que zone
+			// element autre que zone
 }
 
 	return 0;
