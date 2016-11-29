c5a3a0b6ec716deba4262796104dbc0c60b706ef
cruchet <vassili.cruchet@epfl.ch>
2016-11-29 11:25:38 +0100

    nb_tic dans xml

--- src/src/main.cpp
+++ src/src/main.cpp
@@ -24,17 +24,18 @@
 using namespace Domotique;
 
 void lecture_xml(string nom_fichier,vector<vector<Processus*> >* paysage,
-		vector<string>* nom_zone, vector<double>* etat_initial);
+		vector<string>* nom_zone, vector<double>* etat_initial, unsigned int* nb_tic);
 
 int main(void) {
 	srand(std::time(NULL));
 	vector<vector<Processus*> > paysage;
 	vector<string> nom_zone;
 	vector<double> etat_initial;
-	lecture_xml("paysage.xml", &paysage, &nom_zone, &etat_initial);
+	unsigned int nb_tic;
+	lecture_xml("paysage.xml", &paysage, &nom_zone, &etat_initial, &nb_tic);
 
 	//----- Creation du simulateur et lancement de la simulation ------//
-	Sim * sim = new Sim(paysage, paysage.size(), 20);
+	Sim * sim = new Sim(paysage, paysage.size(), nb_tic);
 
 	vector<double> setting;
 	setting.push_back(paysage.size()); //nb_zone
@@ -45,69 +46,73 @@ int main(void) {
 }
 
 void lecture_xml(string nom_fichier,vector<vector<Processus*> >* paysage,
-		vector<string>* nom_zone, vector<double>* etat_initial){
-		//-------------LECTURE DU FICHIER XML ET GESTION ERREUR -------------------
-		TiXmlDocument doc_=nom_fichier;
-		bool loadOkay = doc_.LoadFile();
-		if ( !loadOkay ) {
-			printf( "Lecture impossible du fichier 'paysage_.xml'. "
-					"Error='%s' \n", doc_.ErrorDesc() );
-			exit( 1 );
-		}
-		else {
-			printf( "Lecture correcte du fichier 'paysage_.xml'.\n");
-		}
-		TiXmlElement* child1 = doc_.FirstChildElement();
+		vector<string>* nom_zone, vector<double>* etat_initial, unsigned int* nb_tic){
+	//-------------LECTURE DU FICHIER XML ET GESTION ERREUR -------------------
+	TiXmlDocument doc_=nom_fichier;
+	bool loadOkay = doc_.LoadFile();
+	if ( !loadOkay ) {
+		printf( "Lecture impossible du fichier 'paysage_.xml'. "
+				"Error='%s' \n", doc_.ErrorDesc() );
+		exit( 1 );
+	}
+	else {
+		printf( "Lecture correcte du fichier 'paysage_.xml'.\n");
+	}
+	TiXmlElement* child1 = doc_.FirstChildElement();
 
-		//--------------------- EXTRACTION DES TRIPLETS: NIVEAU 1
-		for(TiXmlElement* child2 = child1->FirstChild("zone")->ToElement(); child2; child2=child2->NextSiblingElement())
-		{ 		//BOUCLE pour extraire l'ensemble des triplets "zone" du paysage_
+	//--------------------- EXTRACTION DES TRIPLETS: NIVEAU 1
+	for(TiXmlElement* child2 = child1->FirstChild("zone")->ToElement(); child2; child2=child2->NextSiblingElement())
+	{ 		//BOUCLE pour extraire l'ensemble des triplets "zone" du paysage_
 
-			if (strcmp(child2->Value(),"zone")==0){
-				vector<Processus*> zone(0);
-				nom_zone->push_back(child2->Attribute( "nom"));
-				//int id_zone = get_attr_int(child2,"ID", true, 0);
+		if (strcmp(child2->Value(),"zone")==0){
+			vector<Processus*> zone(0);
+			nom_zone->push_back(child2->Attribute( "nom"));
+			//int id_zone = get_attr_int(child2,"ID", true, 0);
 
-				// EXTRACTION DU PHENOMENE: NIVEAU 2
-				TiXmlElement* child3 = child2->FirstChild("phenomene")->ToElement () ;
-				string nom_phen = child3->Attribute( "nom");
-				string mode_phen=  child3->Attribute( "mode");
-				if (strcmp(child3->Attribute("mode"),"aleatoire")==0){
-					TiXmlElement* child4 = child3->FirstChild("parametres")->ToElement();
-					vector<double> phen_param(2);
-					phen_param.at(0) = get_attr_dbl(child4,"valmin", true, 1);
-					phen_param.at(1) =  get_attr_dbl(child4,"valmax", true, 10);
-					Phenomene* phen = new Phenomene(nom_phen, mode_phen, phen_param);
-					zone.push_back(phen);
-				}
-
-				// EXTRACTION DU CONTROLE DE LA ZONE: NIVEAU 2
-				TiXmlElement* child5 = child2->FirstChild("control")->ToElement();
-				vector<double> param_ctrl(0);
-				string nom_ctrl = child5->Attribute( "nom");
-				string mode_ctrl=  child5->Attribute( "mode");
-				if (strcmp(child5->Attribute("mode"),"saturation")==0){
-					TiXmlElement* child7 = child5->FirstChild("parametres")->ToElement();
-					param_ctrl.push_back (get_attr_dbl(child7,"valsat", true, 1));
-				}
-				Control* ctrl = new Control(nom_ctrl, mode_ctrl, param_ctrl);
-				zone.push_back(ctrl);
-
-				// EXTRACTION DE l'ETAT DE LA ZONE: NIVEAU 2
-				TiXmlElement* child6 = child2->FirstChild("etat")->ToElement();
-				string nom_etat = child6->Attribute( "nom");
-				etat_initial->push_back (get_attr_dbl(child6,"etat_initial"));
-				vector<double> param_etat(2);
-				param_etat.at(0)= get_attr_dbl(child6,"Iphen", true, 1);
-				param_etat.at(1)= get_attr_dbl(child6,"Ictrl", true, 1);
-				Etat* etat = new Etat(nom_etat, param_etat);
-				zone.push_back(etat);
-
-				paysage->push_back(zone);
-
+			// EXTRACTION DU PHENOMENE: NIVEAU 2
+			TiXmlElement* child3 = child2->FirstChild("phenomene")->ToElement () ;
+			string nom_phen = child3->Attribute( "nom");
+			string mode_phen=  child3->Attribute( "mode");
+			if (strcmp(child3->Attribute("mode"),"aleatoire")==0){
+				TiXmlElement* child4 = child3->FirstChild("parametres")->ToElement();
+				vector<double> phen_param(2);
+				phen_param.at(0) = get_attr_dbl(child4,"valmin", true, 1);
+				phen_param.at(1) =  get_attr_dbl(child4,"valmax", true, 10);
+				Phenomene* phen = new Phenomene(nom_phen, mode_phen, phen_param);
+				zone.push_back(phen);
 			}
-			else {cout << "Ce n'est pas une zone, il s'agit de la balise : " << child2->Value() << endl;}
-			// element autre que zone
+
+			// EXTRACTION DU CONTROLE DE LA ZONE: NIVEAU 2
+			TiXmlElement* child5 = child2->FirstChild("control")->ToElement();
+			vector<double> param_ctrl(0);
+			string nom_ctrl = child5->Attribute( "nom");
+			string mode_ctrl=  child5->Attribute( "mode");
+			if (strcmp(child5->Attribute("mode"),"saturation")==0){
+				TiXmlElement* child7 = child5->FirstChild("parametres")->ToElement();
+				param_ctrl.push_back (get_attr_dbl(child7,"valsat", true, 1));
 			}
+			Control* ctrl = new Control(nom_ctrl, mode_ctrl, param_ctrl);
+			zone.push_back(ctrl);
+
+			// EXTRACTION DE l'ETAT DE LA ZONE: NIVEAU 2
+			TiXmlElement* child6 = child2->FirstChild("etat")->ToElement();
+			string nom_etat = child6->Attribute( "nom");
+			etat_initial->push_back (get_attr_dbl(child6,"etat_initial"));
+			vector<double> param_etat(2);
+			param_etat.at(0)= get_attr_dbl(child6,"Iphen", true, 1);
+			param_etat.at(1)= get_attr_dbl(child6,"Ictrl", true, 1);
+			Etat* etat = new Etat(nom_etat, param_etat);
+			zone.push_back(etat);
+
+			paysage->push_back(zone);
+
+		}
+		else {cout << "Ce n'est pas une zone, il s'agit de la balise : " << child2->Value() << endl;}
+		// element autre que zone
+	}
+	TiXmlElement* child1_2 = child1->NextSiblingElement();
+	*nb_tic = get_attr_int(child1_2,"nb_tic", true, 1);
+
+
 
 }
