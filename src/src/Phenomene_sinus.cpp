20d00186e1229d79245d2e846f88652b98c50989
jordan Metz <jordan.metz@epfl.ch>
2016-12-07 17:18:14 +0100

    phen sin

--- src/src/Phenomene_sinus.cpp
+++ src/src/Phenomene_sinus.cpp
@@ -12,9 +12,9 @@ using namespace std;
 namespace Domotique {
 
 Phenomene_sinus::Phenomene_sinus(string nom, double ampl, long int period,
-		double sat_max= std::numeric_limits<double>::infinity(),
-		double sat_min = -1* std::numeric_limits<double>::infinity(),
-		double offs = 0.0, long int phase=0): Phenomene(nom), ampl_(ampl), period_(period),
+		double sat_max,
+		double sat_min,
+		double offs, long int phase): Phenomene(nom), ampl_(ampl), period_(period),
 		sat_max_(sat_max), sat_min_(sat_min), offs_(offs), phase_(phase){
 	cout<< "[Phenomene_sinus] Création de "<< nom << endl;
 	assert(period >0);
