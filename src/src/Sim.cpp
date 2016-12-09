200070a0d73ba436faf7f720694f1eb0f917e74e
cruchet <vassili.cruchet@epfl.ch>
2016-12-09 10:25:29 +0100

    debug..

--- src/src/Sim.cpp
+++ src/src/Sim.cpp
@@ -20,13 +20,14 @@ Sim::Sim(unsigned int max_tic): ntic_(max_tic), tic_(0){}
 Sim::~Sim() {}
 
 void Sim::run(void) {
-	cout << "#Lancement de la simulation:" << endl<<endl;
+	cout << "#Lancement de la simulation" << endl<<endl;
 
 	for(tic_=0; tic_<=ntic_;tic_++) {
 		for(unsigned int i=0; i<process_.size(); i++){
-			(process_.at(tic_))->run(tic_);
+			(process_.at(i))->run(tic_);
 		}
 	}
+	cout << "#Fin de la simulation" << endl;
 }
 
 } /* namespace Domotique */
